57bedd17d45fe88ee7d5d0ea498235f4763703a4
FKatherina <114523268+FKatherina@users.noreply.github.com>
2023-05-16 20:58:16 +0300

    Update [hw]03_task_string.py

--- Module4/home_work/01_string/[hw]03_task_string.py
+++ Module4/home_work/01_string/[hw]03_task_string.py
@@ -1,3 +1,27 @@
 # Посчитайте количество согласных букв в данной строке.
 
 text = ...
+text = (input("Введите текст: "))
+
+letter_a = text.count('а')
+letter_e = text.count('е')
+letter_o = text.count('о')
+letter_y = text.count('у')
+letter_ay = text.count('ю')
+letter_ey = text.count('ё')
+letter_ye = text.count('э')
+letter_ee = text.count('и')
+letter_yy = text.count('ы')
+letter_eya = text.count('я')
+
+Summa = letter_eya+letter_yy+letter_ee+letter_ye+letter_ey+letter_e+letter_a+letter_ay+letter_y+letter_o
+
+print("Глассных букв - ", Summa )
+# Лучший вариант
+s = text
+count = 0
+vowels = set("ёуеыаоэяию")
+for letter in s:
+    if letter in vowels:
+        count += 1
+print("Количество гласных равно:", count)
