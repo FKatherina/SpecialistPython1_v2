adf21acfcdb288afdd3e12a7fef8deb31f38e0e8
FKatherina <114523268+FKatherina@users.noreply.github.com>
2023-05-16 20:22:25 +0300

    Update [hw]01_task_string.py

--- Module4/home_work/01_string/[hw]01_task_string.py
+++ Module4/home_work/01_string/[hw]01_task_string.py
@@ -2,3 +2,14 @@
 # Если точек и запятых одинаково - выведите "одинаково"
 
 text = ...
+text = (input("Введите текст: "))
+
+dot = text.count('.')
+comma = text.count(',')
+
+if dot>comma:
+    print("Больше знаков '.' - ", dot )
+elif comma>dot:
+    print("Больше знаков ',' - ", comma)
+elif comma == dot:
+    print("Одинаково
